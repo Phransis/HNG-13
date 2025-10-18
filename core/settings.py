883c5429e8ad4d9f169fe04ae0224c87c69623a3
Handla <duofrancis11@gmail.com>
2025-10-18 09:19:25 +0000

    updated my procfile

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s)yvm252af4e=^2jegi7@!84%#w3a6#kilv6x1@btr$n7)rwm%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1',]
 
 
 # Application definition
